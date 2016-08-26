1c464ae092cf30cd58923150d39fe5395ebbfcd3
Sergey <rudserg@gmail.com>
2016-08-26 15:19:20 +0300

    add languages

--- design/js/admin/config.js
+++ design/js/admin/config.js
@@ -1,5 +1,7 @@
 var LANGUAGES = [
     {language_id:1, title:"Русский", alias:"ru", is_default:1},
     {language_id:2, title:"English", alias:"en", is_default:0},
-    {language_id:3, title:"Français", alias:"fr", is_default:0}
+    {language_id:3, title:"Français", alias:"fr", is_default:0},
+    {language_id:4, title:"العربية", alias:"ar", is_default:0},
+    {language_id:5, title:"Español", alias:"es", is_default:0}
 ];
